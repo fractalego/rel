51959d46a131792110caa9deebbffeaa247ae5f9
alberto cetoli <alberto.cetoli@nlulite.com>
2019-03-09 16:39:07 +0000

    Using two rules and two sentences

--- rel_extract/train.py
+++ rel_extract/train.py
@@ -13,6 +13,6 @@ set_global_device('cpu')
 
 if __name__ == '__main__':
     dgt = DGT(_metric)
-    dgt.fit(json.load(open(_gradient_test_filename)), epochs=500, step=1e-2)
+    dgt.fit(json.load(open(_gradient_test_filename)), epochs=20, step=1e-2)
     print(dgt.get_all_rules_with_weights())
     dgt.save(open(os.path.join(_path, '../data/saved.json'), 'w'))
