89f864f8b33dc11fc957fcf13445e4786c7777ff
alberto cetoli <alberto.cetoli@nlulite.com>
2019-07-13 19:00:14 +0100

    Iterating through the various matches

--- rel_extract/train.py
+++ rel_extract/train.py
@@ -11,10 +11,11 @@ _path = os.path.dirname(__file__)
 _gradient_test_filename = os.path.join(_path, '../data/training_smallest.json')
 
 _metric = get_metric_or_save_pickle(_path, '../data/glove.txt', '../data/metric.pickle')
+_metric._vector_matching_threshold = 0.7  ### THIS IS BECAUSE 0.6 is too low! Words are matched without making sense
 
 set_global_device('cpu')
 
 if __name__ == '__main__':
     dgt = DGT(_metric, json.load(open(_gradient_test_filename)))
-    dgt.fit(epochs=50, step=1e-2, relaxation_epochs=1, relaxation_step=1e-2)
+    dgt.fit(epochs=50, step=1e-3, relaxation_epochs=0, relaxation_step=1e-2)
     dgt.save(open(os.path.join(_path, '../data/saved.json'), 'w'))
